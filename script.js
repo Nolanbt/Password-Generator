fcce576940c32bcaa6a3ada96b340ea2ac98fdcb
Nolan Terpstra <nbakert@gmail.com>
2020-08-05 23:38:58 -0500

    Randomized password is generated properly, but not displayed on DOM.

--- script.js
+++ script.js
@@ -11,4 +11,88 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); {
+
+  var lowerOptions = "abcdefghijklmnopqrstuvwxyz";
+  var upperOptions = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericOptions = "0123456789";
+  var specialOptions = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  let optionsChoices = [];
+
+  function generatePassword() {
+    console.log("Generate the password")
+    // Length Criteria
+    let passLength = prompt("How many characters do you want in your password?");
+    console.log(passLength);
+
+    if (passLength > 7 && passLength < 129) {
+      console.log(passLength);
+    }
+    else if (passLength < 8 || passLength > 128)
+      if (passLength > 128) {
+        alert("Password must not exceed 128 characters");
+        generatePassword();
+        return;
+      }
+      else if (passLength < 8) {
+        alert("Password must be at least 8 characters long")
+        generatePassword();
+        return;
+      }
+
+    // Lowercase option
+    let passLower = confirm("Do you want lower case characters included?")
+
+    if (passLower == true) {
+      console.log("Include lowecase: " + passLower);
+      optionsChoices.push(lowerOptions);
+    }
+    else {
+      console.log("Include lowecase: " + passLower);
+    }
+
+    // Uppercase option
+    let passUpper = confirm("Do you want upper case characters included?")
+
+    if (passUpper == true) {
+      console.log("Include uppercase: " + passUpper);
+      optionsChoices.push(upperOptions);
+    }
+    else {
+      console.log("Include uppercase: " + passUpper);
+    }
+
+    // Numerical option
+    let passNumeric = confirm("Do you want numbers to be included?")
+
+    if (passNumeric == true) {
+      console.log("Include numbers: " + passNumeric);
+      optionsChoices.push(numericOptions);
+    }
+    else {
+      console.log("Include numbers: " + passNumeric);
+    }
+
+    // Specical characters option
+    let passSpecial = confirm("Do you want special characters included?")
+
+    if (passSpecial == true) {
+      console.log("Include special characters: " + passSpecial);
+      optionsChoices.push(specialOptions);
+    }
+    else {
+      console.log("Include special characters: " + passSpecial);
+    }
+
+    for (let i = 0; i < passLength; i++) {
+      let optionsArray = optionsChoices[Math.floor(Math.random() * optionsChoices.length)];
+      let passSelection = optionsArray.charAt(Math.floor(Math.random() * optionsArray.length));
+      console.log(passSelection);
+      // Put all the values together into one line to make the password
+    }
+
+  }
+
+
+}
+
